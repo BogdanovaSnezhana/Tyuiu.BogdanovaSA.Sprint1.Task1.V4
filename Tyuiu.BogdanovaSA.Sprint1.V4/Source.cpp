10b900b560d5626f4c03a8559aca7b041c2e5438
tyuiu.bogdanovasnezhana@outlook.com <bogdanova.snezha09@gmail.com>
2023-09-19 22:25:44 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.BogdanovaSA.Sprint1.V4/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int number;
+	cout << "Введите целое число: ";
+	cin>>  number;
+	if (number % 10 == 7)
+	{
+		cout << "Число оканчивается на 7." << endl;
+	}
+	else {
+		cout << "Число не оканчивается на 7." << endl;
+	}
+	return 0;
+}
